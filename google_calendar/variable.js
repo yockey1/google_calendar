bf65545d1345fa800fd72ab82df60d8eb4aef7fb
yockey1 <108849047+yockey1@users.noreply.github.com>
2023-05-16 10:35:10 +0900

    Update variable.js

--- google_calendar/variable.js
+++ google_calendar/variable.js
@@ -1,12 +1,15 @@
+const WEEKDAY = ["日", "月", "火", "水", "木", "金", "土"];
+
+
 //取得したいgooglecalendarのメールアドレス、カレンダーの名前
 const calendarTitleMap = {
-    [PropertiesService.getScriptProperties().getProperty('your_name')]: "○○のカレンダー",
-  };
-  
-  //line_groupが変わったときのみ変更
-  const token = PropertiesService.getScriptProperties().getProperty('line_api_token');
-  
-  //メンバーが変わったときのみ変更
-  const memberList = [
-      {email: PropertiesService.getScriptProperties().getProperty('name'), name: 'name'},
-    ];
+  [PropertiesService.getScriptProperties().getProperty('your_name')]: "○○のカレンダー",
+};
+
+//line_groupが変わったときのみ変更
+const token = PropertiesService.getScriptProperties().getProperty('line_api_token');
+
+//メンバーが変わったときのみ変更
+const memberList = [
+  { email: PropertiesService.getScriptProperties().getProperty('name'), name: 'name' },
+];
